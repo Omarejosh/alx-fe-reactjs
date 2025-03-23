84230b2265b74562a69a74f0027e0ec11ee877fa
Joshua Omare <omarejosh@gmail.com>
2025-03-23 20:09:29 +0100

    files updated

---
+++ alx-react-app-new/src/components/MainContent.jsx
@@ -0,0 +1,13 @@
+const Main = () => {
+  return (
+    <main style={{ backgroundColor: "#f4f4f4", padding: "20px", textAlign: "center" }}>
+      <p style={{ fontSize: "18px", color: "#333" }}>
+        I love to visit New York, Paris, and Tokyo.
+      </p>
+    </main>
+  );
+};
+
+
+
+export default MainContent;
