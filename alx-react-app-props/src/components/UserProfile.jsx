9cca1fca086f4ce4f31a28ffe83e30c3f9db3f82
Joshua Omare <omarejosh@gmail.com>
2025-03-27 22:14:20 +0100

    files updated

--- alx-react-app-props/src/components/UserProfile.jsx
+++ alx-react-app-props/src/components/UserProfile.jsx
@@ -1,13 +1,15 @@
+// UserProfile.jsx
+import { useContext } from "react";
+import UserContext from "./UserContext";
+
 const UserProfile = (props) => {
     return (
       <div>
         <h2>{props.name}</h2>
         <p>Age: {props.age}</p>
         <p>Bio: {props.bio}</p>
-        
       </div>
     );
   };
-  
-  export default UserProfile;
-  
+
+export default UserProfile;
